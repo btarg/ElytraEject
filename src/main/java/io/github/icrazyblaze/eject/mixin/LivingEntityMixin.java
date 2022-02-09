2c2ebb553e10761182b5e8ab3b849a044f809557
iCrazyBlaze <icrazyblaze1@gmail.com>
2022-02-09 19:53:34 +0000

    remove accessor and extend instead

--- src/main/java/io/github/icrazyblaze/eject/mixin/LivingEntityMixin.java
+++ src/main/java/io/github/icrazyblaze/eject/mixin/LivingEntityMixin.java
@@ -1,15 +1,21 @@
 package io.github.icrazyblaze.eject.mixin;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
+public abstract class LivingEntityMixin extends Entity {
 
+    public LivingEntityMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
 
     @Inject(method = "updateFallFlying", at = @At("HEAD"), cancellable = true)
     private void injectTick(CallbackInfo ci) {
@@ -17,7 +23,7 @@ public abstract class LivingEntityMixin {
         LivingEntity player = (LivingEntity) (Object) this;
 
         if (player instanceof ServerPlayer && player.isFallFlying() && player.getFallFlyingTicks() > 20 && player.isShiftKeyDown()) {
-            ((EntityAccessor) player).setSharedFlagInvoker(7, false);
+            this.setSharedFlag(7, false);
             ci.cancel();
         }
 
